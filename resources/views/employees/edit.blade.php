b0b32e24bc2f227a3fe92dc7b5f2e701dd4973ef
Nikos Douras <139460420+NikosDouras@users.noreply.github.com>
2025-09-04 12:51:20 +0300

    Add Greek and English localization with language switcher

--- resources/views/employees/edit.blade.php
+++ resources/views/employees/edit.blade.php
@@ -1,24 +1,24 @@
 <x-app-layout>
-    <x-slot name="header"><h2 class="font-semibold text-xl">Edit Employee</h2></x-slot>
+    <x-slot name="header"><h2 class="font-semibold text-xl">{{ __('Edit Employee') }}</h2></x-slot>
 
     <div class="p-6 max-w-xl">
         <form method="POST" action="{{ route('employees.update', $employee) }}">
             @csrf @method('PUT')
 
             <div class="mb-4">
-                <label class="block">First name *</label>
+                <label class="block">{{ __('First name') }} *</label>
                 <input name="first_name" class="border w-full" value="{{ old('first_name', $employee->first_name) }}">
                 <x-input-error :messages="$errors->get('first_name')" class="mt-2" />
             </div>
 
             <div class="mb-4">
-                <label class="block">Last name *</label>
+                <label class="block">{{ __('Last name') }} *</label>
                 <input name="last_name" class="border w-full" value="{{ old('last_name', $employee->last_name) }}">
                 <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
             </div>
 
             <div class="mb-4">
-                <label class="block">Company *</label>
+                <label class="block">{{ __('Company') }} *</label>
                 <select name="company_id" class="border w-full">
                     @foreach($companies as $id => $name)
                         <option value="{{ $id }}" @selected(old('company_id', $employee->company_id) == $id)>{{ $name }}</option>
@@ -28,18 +28,18 @@
             </div>
 
             <div class="mb-4">
-                <label class="block">Email</label>
+                <label class="block">{{ __('Email') }}</label>
                 <input name="email" class="border w-full" value="{{ old('email', $employee->email) }}">
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
 
             <div class="mb-4">
-                <label class="block">Phone</label>
+                <label class="block">{{ __('Phone') }}</label>
                 <input name="phone" class="border w-full" value="{{ old('phone', $employee->phone) }}">
                 <x-input-error :messages="$errors->get('phone')" class="mt-2" />
             </div>
 
-            <x-primary-button>Update</x-primary-button>
+            <x-primary-button>{{ __('Update') }}</x-primary-button>
         </form>
     </div>
 </x-app-layout>
